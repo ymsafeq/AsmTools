6bfa684b95a7f4ef5558f8fe23b320eb665a0be7
user <user@example.com>
2018-02-04 01:00:52 +0900

    add GetAssemblyVersion

---
+++ GetAssemblyVersion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace GetAssemblyVersion
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length != 1)
+                {
+                    throw new ArgumentException("One Assembly File Path is needed.");
+                }
+
+                string path = args[0];
+                Console.WriteLine(Assembly.LoadFrom(path).GetName().Version);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+    }
+}
